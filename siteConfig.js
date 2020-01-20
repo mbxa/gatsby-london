b7b101cf12f62578e8c25423e32c73e0529a4e46
mbxa <59708452+mbxa@users.noreply.github.com>
2020-01-21 00:37:25 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
+  name: '75396734',
+  shortName: '75396734',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
   url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
   prefix: '/',
